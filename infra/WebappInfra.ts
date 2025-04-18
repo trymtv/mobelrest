d370b059b61aab5861ab2f55b6fd9129e0951824
TrymT <tiltrym@gmail.com>
2025-04-18 18:28:26 +0200

    fix spa routing for s3

--- infra/WebappInfra.ts
+++ infra/WebappInfra.ts
@@ -45,26 +45,25 @@ export class Webapp extends cdk.Stack {
     );
 
     const s3Bucket = new cdk.aws_s3.Bucket(this, "s3-bucket-content", {
-      blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
+      blockPublicAccess: new cdk.aws_s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+        blockPublicPolicy: false,
+      }),
       encryption: cdk.aws_s3.BucketEncryption.S3_MANAGED,
       enforceSSL: true,
       removalPolicy: cdk.RemovalPolicy.RETAIN,
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "index.html",
     });
 
     s3Bucket.grantReadWrite(githubActionsRole);
 
-    const originAccessIdentity = new cdk.aws_cloudfront.OriginAccessIdentity(
-      this,
-      "cloudfront-origin-access-identity"
-    );
-    s3Bucket.grantRead(originAccessIdentity);
+    const cloudfrontS3Origin =
+      new cdk.aws_cloudfront_origins.S3StaticWebsiteOrigin(s3Bucket);
 
-    const cloudfrontS3Origin = new cdk.aws_cloudfront_origins.S3Origin(
-      s3Bucket,
-      {
-        originAccessIdentity: originAccessIdentity,
-      }
-    );
     const distribution = new cdk.aws_cloudfront.Distribution(
       this,
       "cloudfront-content-distribution",
