5833170290da6a871e68577ac0d65db9a155bf0b
TrymT <tiltrym@gmail.com>
2026-01-18 19:50:31 +0100

    redirect all to home

--- app/routes.ts
+++ app/routes.ts
@@ -1,11 +1,14 @@
 import {
   type RouteConfig,
   index,
-  layout,
+  layout, route,
 } from "@react-router/dev/routes";
 
 export default [
   layout("layout/index.tsx", [
       index("routes/home.tsx"),
+      route("*", "routes/home.tsx", {
+        id: "not-found",
+      })
   ]),
 ] satisfies RouteConfig;
